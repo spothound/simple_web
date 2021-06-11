4d6f143c19edb96d380a2d89eb952444794c782d
Paco <navarromoralesdev@gmail.com>
2021-06-11 15:46:36 +0200

    Add gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,22 +9,22 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
-resource "aws_key_pair" "test" {
-  key_name   = "test"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQChAuN3EpJjAYcF1FoAEVN1smJvPJvqeRqBEFhgbjfn9cSMaB35Puygs8qQT5VNxdhfSDQUsLOl1kxrNK1+w92VCuQ1zqJR5C7rbnAvYvCUsgSiXCzkFRz1tjIO4fJ4nt1QvqMYyIROdkA6+srLgbpeNmJpL9m9LWJ58bA5pnpOI4kIg84Dy52FOn93cXRJF+csN/+2gJrWuaULLitBnoM+kbBv+7S4lJ7r8NvmNUwePBNGPOL26dvMbD3n1hP8DKR2HWQdIeE1O+pd3Uf+W2f3Xc2gDuviPhEj3bZoHtegMlH7Y1lcRFwCuGrNRAlZGU9dMqLO3LQACgU7XbkUtkd37O4H+TCi8lOlODz2M8iaHTCfDSJE8/BSV/3jcRroyCTwkLQ687pXJIviPMJtJ2FpRTu8kD4cEXodCnqoH8oLY7rYr7bk1XQRL6EZJoIzgrhbLoDczVuMwV/6d/ISzsdU8NnXlPXqfNGyhPLtsfzae6PXbo1G7B3Lfl+OIwQzaiU= grg121@archlinux"
+resource "aws_key_pair" "ansible" {
+  key_name   = "ansible"
+  public_key = file("ansible.pub")
 }
 
 provider "aws" {
   profile    = "default"
   region     = "us-west-2"
-  access_key = "AKIAU7V2EO6JBCTCDLH5"
-  secret_key = "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"
+  access_key = file("aws_access_key")
+  secret_key = file("aws_secret_key")
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0800fc0fa715fdcfe"
   instance_type = "t2.micro"
-  key_name      = "default"
+  key_name      = "ansible"
   tags = {
     Name = "dev w devops simple_web"
   }
